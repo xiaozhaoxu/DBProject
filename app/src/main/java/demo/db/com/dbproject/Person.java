0afc685bab0e0a844b175180d9d0543ddbc20ed0
zhaoxu <330868352@qq.com>
2016-08-26 11:38:28 +0800

    first commit

---
+++ app/src/main/java/demo/db/com/dbproject/Person.java
@@ -0,0 +1,12 @@
+package demo.db.com.dbproject;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by zhaoxu2014 on 16/8/26.
+ */
+public class Person extends SugarRecord {
+    String name;
+    int age;
+
+}
